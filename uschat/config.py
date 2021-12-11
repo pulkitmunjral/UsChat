ed3a10e13b4dc3702925e28189e22821fdb45a84
pulkit munjral <44071448+pulkitmunjral@users.noreply.github.com>
2021-12-11 19:51:56 +0530

    Update config.py

--- uschat/config.py
+++ uschat/config.py
@@ -2,20 +2,11 @@ import os
 
 
 class Config:
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    # MAIL_SERVER = 'smtp.googlemail.com'
-    # MAIL_PORT = 587
-    # MAIL_USE_TLS = True
-    # MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    # MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-
-    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'uschat.offical@gmail.com'
-    MAIL_PASSWORD = 'kyondssan@0406'
-    DEBUG = True
-    ALLOWED_HOSTS = ['uschatapplication.herokuapp.com']
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
